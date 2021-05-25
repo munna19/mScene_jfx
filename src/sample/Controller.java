cefbc9df3f2e6ca89ca3b8224fdfc589d2e844cd
munna19 <abdulalmamun971@yahoo.com>
2021-05-25 22:55:30 +0600

    Mscene

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+
+    @FXML
+    void BackButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoButtonAction(ActionEvent event) {
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
